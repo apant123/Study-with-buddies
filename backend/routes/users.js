ad9c586f25238371caa9ce52bef98af0ebbe3861
Anish Goswami <anishgoswami@Anishs-MacBook-Pro.local>
2024-05-30 17:20:08 -0700

    adding url requests with corresponding function calls to backend for user

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -14,3 +14,18 @@ const { loginUser,
     updateProfile, 
     removeEvent} = require('../controllers/userController')
 
+route.get('/', getAllGroups)
+
+route.get('/:id', getGroup)
+    
+route.post('/', createGroup)
+    
+route.delete('/:id', deleteGroup)
+    
+route.patch('/:id', updateGroup)
+    
+module.exports = route
+
+
+
+
