7e6c815f42cec8c9eacdd12b7d06a3b5b9368d23
Arav Pant <aravpant17@gmail.com>
2024-05-31 14:04:07 -0700

    Added Packages + bug fixing

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,46 +1,39 @@
-const express = require('express')
-const route  = express.Router()
+const express = require('express');
+const route = express.Router();
 const cors = require('cors');
 
 // controller functions
-const { loginUser, 
-    signupUser, 
-    getUsers, 
-    updateUser, 
-    addEvent,  
-    getGroup, 
-    getAllUsers, 
-    getUserById, 
-    updateProfile, 
-    removeGroup} = require('../Controller/usercontroller')
+const {
+  loginUser,
+  signupUser,
+  getUsers,
+  updateUser,
+  addEvent,
+  getUserGroup, // Assuming this is imported correctly
+  getAllUsers,
+  getUserById,
+  updateProfile,
+  removeGroup
+} = require('../Controller/usercontroller');
 
+route.post('/login', loginUser);
 
-route.post('/login', loginUser)
+route.post('/signup', signupUser);
 
+route.post('/getUsers', cors(), getUsers);
 
-route.post('/signup', signupUser)
-      
-route.post('/getUsers', cors(), getUsers)
-    
-    
-route.patch('/updateUser', updateUser)
-      
-route.patch('/addEvent/:id', addEvent)
-      
-route.patch('/removeEvent/:id', removeGroup)
-      
-route.get('/getUserEvents/:id', getUserGroup)
-      
-route.get('/getAllUsers', getAllUsers)
-      
-route.get('/getUserByID/:id', getUserById)
-      
-route.patch('/updateProfile/:id', updateProfile)
-      
-module.exports = route   
+route.patch('/updateUser', updateUser);
 
+route.patch('/addEvent/:id', addEvent);
 
+route.patch('/removeEvent/:id', removeGroup);
 
+route.get('/getUserEvents/:id', getUserGroup); // Assuming this is the correct function name
 
+route.get('/getAllUsers', getAllUsers);
 
+route.get('/getUserByID/:id', getUserById);
 
+route.patch('/updateProfile/:id', updateProfile);
+
+module.exports = route;
