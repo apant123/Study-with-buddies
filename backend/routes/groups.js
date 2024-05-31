f02b6e939474cbfdf99d4d2422cac35d96815424
Anish Goswami <anishgoswami@Anishs-MacBook-Pro.local>
2024-05-30 17:35:17 -0700

    adding url requests with corresponding function calls to backend for groups

--- backend/routes/groups.js
+++ backend/routes/groups.js
@@ -15,3 +15,18 @@ const {
 } = require('../Controller/groupcontroller')
 
 
+
+route.get('/', getAllGroups)
+
+
+route.get('/:id', getGroup)
+  
+route.post('/', createGroup)
+  
+route.delete('/:id', deleteGroup)
+  
+route.patch('/:id', updateGroup)
+  
+module.exports = route
+
+
