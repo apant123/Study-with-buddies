dc23b4ba49be97cab8526c86061287345c0f693d
Anish Goswami <anishgoswami@Anishs-MacBook-Pro.local>
2024-05-30 17:34:40 -0700

    starting express code for functions related to groups

--- backend/routes/groups.js
+++ backend/routes/groups.js
@@ -1,3 +1,17 @@
 
+const express = require('express');
+const Event = require('../models/eventModel')
+
+
+const route = express.Router()
+
+
+const {
+   createGroup,
+   getGroup,
+   getAllGroups,
+   deleteGroup,
+   updateGroup
+} = require('../Controller/groupcontroller')
 
 
