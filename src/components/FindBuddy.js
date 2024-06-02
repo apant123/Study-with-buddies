6621bb0ea99ea65e985ab4ea28137f9735a6e184
KKumanan <krishna.kumanan@gmail.com>
2024-06-02 00:52:13 -0700

    find groups skeleton

--- src/components/FindBuddy.js
+++ src/components/FindBuddy.js
@@ -3,6 +3,25 @@ import { Button } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import './styles.css';
 
+// async function searchGroups() {
+//   try {
+//       // Connect to the MongoDB server
+//       await client.connect();
+//       console.log('Connected successfully to server');
+
+//       const db = client.db(dbName);
+//       const collection = db.collection('your_collection_name');
+//       const courseFilter = { course: 'YourCourseName' };
+      
+//       const results = await collection.find(courseFilter).toArray();
+      
+//       console.log('Found documents:', results);
+//   } finally {
+
+//       await client.close();
+//   }
+// }
+
 function FindBuddy() {
   const navigate = useNavigate();
   const [loggedIn, setLoggedIn] = useState(false); // is user logged in?
