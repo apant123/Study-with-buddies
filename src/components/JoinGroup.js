6a258f9af43e7bdad1777ffa7babd80f5185e8b5
Sunny Vinay <sunnyvinay7@gmail.com>
2024-06-02 20:16:25 -0700

    JoinGroup layout

--- src/components/JoinGroup.js
+++ src/components/JoinGroup.js
@@ -1,17 +1,58 @@
 import React, { useState, useEffect } from 'react';
-import { Button } from '@mui/material';
+import { Button, Stack, InputLabel, MenuItem, Select, Chip, FormControl} from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 import { useNavigate } from 'react-router-dom';
 import './styles.css';
 import { useAuthContext } from "../hooks/useAuthContext";
+import courseOptions from './courseOptions';
 
 function JoinGroup() {
   const navigate = useNavigate();
   const {user} = useAuthContext();
 
+  const theme = useTheme();
+  const [course, setCourse] = React.useState('');
+
+  const handleChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setCourse(value);
+  };
+
+  const findGroup = (course) => {
+    // Find group from given course
+  };
+
   return (
     <>
         {user ? 
-          <p>You are logged in.</p>  
+          <div style={{ marginTop: 40, textAlign: 'center' }}>
+            <h2>Join a group by selecting your class below</h2>
+            <Stack direction="row" justifyContent="center" alignItems="center" spacing={5}>
+              <FormControl sx={{width:1000}}>
+                <InputLabel id="demo-simple-select-label">Course</InputLabel>
+                <Select
+                  labelId="demo-simple-select-label"
+                  id="demo-simple-select"
+                  value={course}
+                  label="Course"
+                  onChange={handleChange}
+                >
+                  {courseOptions.map((course) => (
+                  <MenuItem
+                    key={course.value}
+                    value={course.label}
+                  >
+                    {course.label}
+                  </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+
+              <Button variant="contained" color="primary" size="large" onClick={findGroup(course)}>Find Group</Button>
+            </Stack>
+          </div> 
         : 
         <div style={{ marginTop: 40, textAlign: 'center' }}>
           <p style={{fontSize: 30}}><b>You are not logged in. Please login to join study groups!</b></p>
